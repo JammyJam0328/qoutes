7f026cf78af3ecad8e821acb7b7dfddcdfc8d898
norjamillekasan <88083841+norjamillekasan@users.noreply.github.com>
2021-09-08 20:46:47 +0800

    1

---
+++ components/Qoutes.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div
+        class="
+          relative
+          py-24
+          px-8
+          bg-transparent
+          rounded-xl
+          overflow-hidden
+          lg:px-16 lg:grid lg:grid-cols-2 lg:gap-x-8
+        "
+      >
+        <div
+          class="
+            absolute
+            inset-0
+            opacity-50
+            filter
+            saturate-0
+            mix-blend-multiply
+          "
+        >
+          <img
+            :src="url + qoute.content"
+            alt=""
+            class="w-full h-full object-cover"
+          />
+        </div>
+        <div class="relative lg:col-span-1">
+          <blockquote class="mt-6 text-white">
+            <p class="text-xl font-medium sm:text-2xl italic">
+              "{{ qoute.content }}"
+            </p>
+            <template v-if="qoute.author">
+              <footer class="mt-6">
+                <p class="flex flex-col font-medium">
+                  <span>- {{ qoute.author }}</span>
+                </p>
+              </footer>
+            </template>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  props: ['qoute', 'img'],
+  data() {
+    return {
+      url: 'https://source.unsplash.com/random/?nature,',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-3xl mx-auto pt-10">
+      <qoutes :qoute="qoutes" :img="this.randomBgImg" />
+      <div class="py-36 flex justify-center cursor-pointer">
+        <span @click.prevent="randomQuote" class="text-xl text-gray-600"
+          >next &#8250;</span
+        >
+      </div>
+      <footer class="text-center text-gray-500">
+        <a
+          href="https://norjamillekasan.netlify.app"
+          target="_blank"
+          class="underline"
+          >Developer</a
+        >
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import Qoutes from '../components/Qoutes.vue'
+export default {
+  components: {
+    Qoutes,
+  },
+  data() {
+    return {
+      randomBgImg: '',
+      tags: ['mountain', 'inspirational', 'nature', 'city', 'love'],
+      randomTag: '',
+      qoutes: '',
+      picked: '',
+    }
+  },
+
+  methods: {
+    async randomQuote() {
+      const response = await fetch('https://api.quotable.io/random')
+      const data = await response.json()
+
+      return (this.qoutes = data)
+    },
+  },
+  mounted() {
+    this.randomQuote()
+  },
+}
+</script>
+
+<style>
+body {
+  background-color: #eec9d2;
+}
+</style>
